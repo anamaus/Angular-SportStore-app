1b8bc68a7a1137b4defe4f5bd378a4d099c727c3
Ana <anamil2112@gmail.com>
2016-08-24 13:18:29 +0200

    add to cart button added

--- angularjs/controllers/productListControllers.js
+++ angularjs/controllers/productListControllers.js
@@ -2,7 +2,7 @@
 angular.module("sportsStore")
     .constant("productListActiveClass", "btn-primary")
     .constant("productListPageCount", 3)
-    .controller("productListCtrl", function ($scope, $filter, productListActiveClass, productListPageCount) {
+    .controller("productListCtrl", function ($scope, $filter, productListActiveClass, productListPageCount, cart) {
 
         var selectedCategory = null;
 
@@ -25,5 +25,8 @@ angular.module("sportsStore")
         }
         $scope.getPageClass = function (page) {
             return $scope.selectedPage == page ? productListActiveClass : "";
+        }
+        $scope.addProductToCart = function (product) {
+            cart.addProduct(product.id, product.name, product.price);
         }
     });
